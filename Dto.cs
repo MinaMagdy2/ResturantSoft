842b1a3e479d34830258ee1d9a48e67bb917169b
MinaMagdy2 <magdymina052@gmail.com>
2025-11-23 14:28:28 +0200

    Add project files.

---
+++ Dto.cs
@@ -0,0 +1,24 @@
+﻿public class OrderItemDto
+{
+    public string FoodName { get; set; }
+    public int Quantity { get; set; }
+    public double Price { get; set; }
+}
+
+public class ResWithOrderItemsDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public decimal Price { get; set; }
+    public string? ImagePath { get; set; }
+    public List<OrderItemDto> OrderItems { get; set; } = new();
+}
+public class OrderWithItemsDto
+{
+    public int Id { get; set; }
+    public string ClientName { get; set; }
+    public int TableNum { get; set; }
+    public DateTime OrderDate { get; set; }
+    public List<OrderItemDto> OrderItems { get; set; } = new();
+}
---
+++ Services/Services.cs
@@ -0,0 +1,24 @@
+﻿using Refit;
+
+public interface IOrderApi
+{
+    // ✅ جلب كل الطلبات
+    [Get("/api/Res")]
+    Task<List<ResWithOrderItemsDto>> GetAllOrders();
+
+    // ✅ جلب الطلب حسب Id
+    [Get("/api/Res/{id}")]
+    Task<ResWithOrderItemsDto> GetOrder(int id);
+
+    // ✅ إنشاء طلب جديد
+    [Post("/api/Res")]
+    Task<ResWithOrderItemsDto> CreateOrder([Body] ResWithOrderItemsDto order);
+
+    // ✅ تعديل طلب
+    [Put("/api/Res/{id}")]
+    Task <ResWithOrderItemsDto> UpdateOrder(int id, [Body] ResWithOrderItemsDto order);
+
+    // ✅ حذف طلب
+    [Delete("/api/Res/{id}")]
+    Task DeleteOrder(int id);
+}
